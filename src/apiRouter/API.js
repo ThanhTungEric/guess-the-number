cc178462ec61a764641da28421cf1502fb483109
thanh <thanh89508@gmail.com>
2024-06-17 22:28:01 +0700

    modify room

--- src/apiRouter/API.js
+++ src/apiRouter/API.js
@@ -1,8 +1,9 @@
-export const host = "http://192.168.1.32:8000";
+export const host = "http://192.168.1.39:8000";
 
 //user
 export const createUserRoute = `${host}/user/create`;
 export const getUserByNameRoute = `${host}/user/userName`;
+export const getUserByIdRoute = `${host}/user/userId`;
 
 //room
 export const createRoomRoute = `${host}/room/create`;
@@ -14,3 +15,4 @@ export const startGameRoute = `${host}/room/startGame`;
 export const guessNumberRoute = `${host}/room/guess`;
 export const leaveRoomRoute = `${host}/room/leave`;
 export const getAllRoomRoute = `${host}/room/get-all`;
+export const deleteAllRoomRoute = `${host}/room/delete-rooms`;
